285ee096448bc5ea54249e3c72735818720a46ef
Daniel Vitor <110069086+danieldevprogrammer@users.noreply.github.com>
2023-12-08 18:56:59 -0300

    Alteração no ex. #002

--- curso-em-video-exercicios-python/#002-respondendo-ao-usuário.py
+++ curso-em-video-exercicios-python/#002-respondendo-ao-usuário.py
@@ -1,4 +1,5 @@
 #Ex. 002 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
 nome = input('Digite seu nume: ')
 #print('É um prazer te conhecer',nome,'!')
 print('É um prazer te conhecer {}!'.format(nome))
