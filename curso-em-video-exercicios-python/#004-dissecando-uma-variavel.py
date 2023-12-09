213b805e11f1470f1ef1ec543fc72655654a6045
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-09 01:37:55 -0300

    Resolução do ex. 004

--- curso-em-video-exercicios-python/#004-dissecando-uma-variavel.py
+++ curso-em-video-exercicios-python/#004-dissecando-uma-variavel.py
@@ -1,1 +1,11 @@
-# Faça um programa que leia algo pelo teclado e mostra na tela o seu tipo primitivo e todas as informações possiveis sobre ela.
+# Ex. 004 - Faça um programa que leia algo pelo teclado e mostra na tela o seu tipo primitivo e todas as informações possiveis sobre ela.
+a = print('Digite algo: ')
+
+print('O tipo primitivo desse valor é', type(a))
+print('Só tem espaços?', a.isspace())
+print('É número?', a.isnumeric())
+print('É alfabético?', a.isalpha())
+print('É alfanumérico?', a.alphanum())
+print('Está em maiúsculas?', a.isupper())
+print('Está em minúscula?', a.islower())
+print('está capitalizada?', a.istitle())
