f29b2ebbf2c00af89e1df5d9b25899f6f43a56a1
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-20 17:03:16 -0300

    Resolução do ex. 07

--- curso-em-video-exercicios-python/#007-media-aritmetica.py
+++ curso-em-video-exercicios-python/#007-media-aritmetica.py
@@ -1,1 +1,8 @@
 # Ex. 007 - Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+grade1 = float(input('Digete a primeira nota: '))
+grade2 = float(input('Digite a segunda nota: '))
+
+arithmeticMean = (grade1 + grade2) / 2
+
+print(
+    f'A média aritmédica entre as notas {grade1:.1f} e {grade2:.1f} é {arithmeticMean:.1f}')
