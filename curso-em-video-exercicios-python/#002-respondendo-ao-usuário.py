4a74c7d7f54758111200a532074e0eff4735f11c
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-20 17:07:27 -0300

    Resolução do ex. 002

--- curso-em-video-exercicios-python/#002-respondendo-ao-usuário.py
+++ curso-em-video-exercicios-python/#002-respondendo-ao-usuário.py
@@ -1,6 +1,6 @@
 # Ex. 002 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 
 name = input('Digite seu nume: ')
-# print('É um prazer te conhecer',nome,'!')
+# print('É um prazer conhecer você',nome,'!')
 # print('É um prazer te conhecer {}!'.format(name))
 print(f'É um prazer te conhecer {name}!')
