5dde27d41938c34c51418221699f617e38a127d0
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-20 16:54:50 -0300

    Resolução do ex. 05

--- curso-em-video-exercicios-python/#005-antecessor-e-sucessor.py
+++ curso-em-video-exercicios-python/#005-antecessor-e-sucessor.py
@@ -1,8 +1,8 @@
 # Ex. 005 - Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
-number1 = int(input('Digite um número: '))
+number = int(input('Digite um número: '))
 
-predecessor = number1 - 1
-successor = number1 + 1
+predecessor = number - 1
+successor = number + 1
 
 print(
-    f'Analisando o número {number1}, o seu antecessor é {predecessor} e o seu sucessor é {successor}')
+    f'Analisando o número {number}, o seu antecessor é {predecessor} e o seu sucessor é {successor}')
