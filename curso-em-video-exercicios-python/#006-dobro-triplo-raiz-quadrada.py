cbf4777c596412d0ee5e8ebddec7df23797c4193
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-20 16:54:25 -0300

    Resolução do ex. 06

--- curso-em-video-exercicios-python/#006-dobro-triplo-raiz-quadrada.py
+++ curso-em-video-exercicios-python/#006-dobro-triplo-raiz-quadrada.py
@@ -1,1 +1,10 @@
 # Ex. 006 - Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+number = int(input('Digite um número: '))
+
+double = number * 2
+triple = number * 3
+squareRoot = number ** (1/2)
+# squareRoot = pow(number, (1/2))
+
+print(
+    f'Analisando o número {number}, \no dobro é {double}, \no triplo é {triple} \ne a raiz quadrada é {squareRoot}')
