f8db704f89e03ec46990583e0d02de5d0eaef11b
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-20 22:11:02 -0300

    Resolução do ex. 010

---
+++ curso-em-video-exercicios-python/#010-conversor-de-moedas.py
@@ -0,0 +1,7 @@
+# Ex. 009 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostra quantos Dólares ela pode comprar.
+# Considere US$1.00 = R$3.27
+real = float(input('Quando dinheiro você tem na carteira? R$'))
+
+dollar = real / 3.27
+
+print(f'Com R${real:.2f} você pode comprar US${dollar:.2f}')
