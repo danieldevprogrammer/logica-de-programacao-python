e5f7bf7e51fd73696bc183859f4ba904ea5716e2
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-22 16:03:33 -0300

    Resolução do ex. 011

--- curso-em-video-exercicios-python/#011-pintando-parede.py
+++ curso-em-video-exercicios-python/#011-pintando-parede.py
@@ -1,8 +1,13 @@
+# Ex. 011 - Faça um programa que leia a largura e a altura de uma parede em metros,
+# calcule a sua área e a quantidade de tinta necessária para pinta-lá, sabendo que cada litro de tinta,
+# pinta uma área de 2m².
 
-width = float(input('Largura da parede: '))
-heigth = float(input('Altura da parede: '))
+width = float(input('Largura da parede em metros: '))
+heigth = float(input('Altura da parede em metros: '))
 area = width * heigth
+
+litersOfPaint = area / 2
 
 print(
     f'Sua parede tem dimensão de {width}mx{heigth}m e sua área é de {area}m².')
-print(f'Para pintar essa parede, você precisará de de tinda.')
+print(f'Para pintar essa parede, você precisará de {litersOfPaint}l de tinda.')
