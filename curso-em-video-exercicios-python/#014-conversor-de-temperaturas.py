d843b77f636e2a3ab73ce5b5d2d2e890d63cb6a2
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-22 16:53:28 -0300

    Resolução do ex. 014

--- curso-em-video-exercicios-python/#014-conversor-de-temperaturas.py
+++ curso-em-video-exercicios-python/#014-conversor-de-temperaturas.py
@@ -1,1 +1,8 @@
-# Ex. 014 -
+# Ex. 014 - Escreva um programa que converta uma temperatura digitada em °C e converta para °F.
+
+temperatureCelsius = float(input('Informe a temperatura em °C: '))
+
+temperatureFahrenheit = ((9 * temperatureCelsius) / 5) + 32
+
+print(
+    f'A temperatura de {temperatureCelsius:.2f}°C corresponde a {temperatureFahrenheit:.2f}°F!')
