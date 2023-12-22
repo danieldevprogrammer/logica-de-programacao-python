db1108c3d4b4709af4ae10b4900981e271aaa9b3
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-22 15:57:00 -0300

    Resolução do ex. 010

--- curso-em-video-exercicios-python/#010-conversor-de-moedas.py
+++ curso-em-video-exercicios-python/#010-conversor-de-moedas.py
@@ -1,4 +1,4 @@
-# Ex. 009 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostra quantos Dólares ela pode comprar.
+# Ex. 010 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostra quantos Dólares ela pode comprar.
 # Considere US$1.00 = R$3.27
 real = float(input('Quando dinheiro você tem na carteira? R$'))
 
---
+++ curso-em-video-exercicios-python/#011-pintando-parede.py
@@ -0,0 +1,8 @@
+
+width = float(input('Largura da parede: '))
+heigth = float(input('Altura da parede: '))
+area = width * heigth
+
+print(
+    f'Sua parede tem dimensão de {width}mx{heigth}m e sua área é de {area}m².')
+print(f'Para pintar essa parede, você precisará de de tinda.')
