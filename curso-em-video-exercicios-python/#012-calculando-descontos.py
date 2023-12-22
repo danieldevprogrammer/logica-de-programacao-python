62c8bb5eae3ff1db46322fd655343e4de52d46c9
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-22 16:35:57 -0300

    Resolução do ex. 012

--- curso-em-video-exercicios-python/#012-calculando-descontos.py
+++ curso-em-video-exercicios-python/#012-calculando-descontos.py
@@ -3,5 +3,5 @@ price = float(input('Qual é o preço do produto? R$'))
 discount = (price * 5 / 100)
 newPrice = price - discount
 
-print()
+print(
     f'O produto que custava R${price:.2f}, na promoção com desconto de 5% vai custar R${newPrice:.2f}.')
