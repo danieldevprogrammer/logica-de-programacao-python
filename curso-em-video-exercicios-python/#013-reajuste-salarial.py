00845073b86fed1d347741f38e6222ea8d1cd027
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-22 16:44:21 -0300

    Resolução do ex. 013

--- curso-em-video-exercicios-python/#013-reajuste-salarial.py
+++ curso-em-video-exercicios-python/#013-reajuste-salarial.py
@@ -1,8 +1,10 @@
-# Ex. 013 -
+# Ex. 013 - Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 wage = float(input('Qual o salário do funcionário? R$'))
-increase = (wage * 15 / 100)
+
+increase = wage * 15 / 100
+
 newWage = wage + increase
 
 
 print(
-    f'Um funcionário que ganhava R${wage}, com 15% de aumento, passa a receber R${newWage}')
+    f'Um funcionário que ganhava R${wage:.2f}, com 15% de aumento, passa a receber R${newWage:.2f}')
