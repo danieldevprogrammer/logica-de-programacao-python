0dcead50c0bdea733e5ebd58d3531d15e3dafb93
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-22 16:35:43 -0300

    Resolução do ex. 012

--- curso-em-video-exercicios-python/#012-calculando-descontos.py
+++ curso-em-video-exercicios-python/#012-calculando-descontos.py
@@ -1,6 +1,7 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 price = float(input('Qual é o preço do produto? R$'))
-discount = price - (price * 5 / 100)
+discount = (price * 5 / 100)
+newPrice = price - discount
 
-print(
-    f'O produto que custava R${price:.2f}, na promoção com desconto de 5% vai custar R${discount:.2f}.')
+print()
+    f'O produto que custava R${price:.2f}, na promoção com desconto de 5% vai custar R${newPrice:.2f}.')
---
+++ curso-em-video-exercicios-python/#013-reajuste-salarial.py
@@ -0,0 +1,5 @@
+
+wage = float(input('Qual o salário do funcionário? R$'))
+
+print(
+    f'Um funcionário que ganhava R${wage}, com 15% de aumento, passa a receber')
