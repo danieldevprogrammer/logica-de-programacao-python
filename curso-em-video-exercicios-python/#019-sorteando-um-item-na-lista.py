a704caccd8d93ce5c40014e2693af5fa315cfc0c
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-25 20:00:03 -0300

    Resolução do ex. 019

--- curso-em-video-exercicios-python/#019-sorteando-um-item-na-lista.py
+++ curso-em-video-exercicios-python/#019-sorteando-um-item-na-lista.py
@@ -1,1 +1,13 @@
-# Ex. 019 -
+# Ex. 019 - Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+from random import choice
+
+firstStudent = str(input('Primeiro aluno: '))
+secondStudent = str(input('Segundo aluno: '))
+thirdStudent = str(input('Terceiro aluno: '))
+fourthStudent = str(input('Quarto aluno: '))
+
+listStudents = [firstStudent, secondStudent, thirdStudent, fourthStudent]
+
+chosenStudent = choice(listStudents)
+
+print(f'O aluno escolhido foi: {chosenStudent}')
