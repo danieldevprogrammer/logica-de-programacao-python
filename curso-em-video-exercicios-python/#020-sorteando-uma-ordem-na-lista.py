d76c4f781ed07a8606075651d60236a656ad7e95
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-25 20:11:59 -0300

    Resolução do ex. 020

--- curso-em-video-exercicios-python/#020-sorteando-uma-ordem-na-lista.py
+++ curso-em-video-exercicios-python/#020-sorteando-uma-ordem-na-lista.py
@@ -1,6 +1,6 @@
 # Ex. 020 - O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
-import random
+# import random
 from random import shuffle
 
 firstStudent = str(input('Primeiro aluno: '))
