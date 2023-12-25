ccc2f0914e2da7e09fe3997e5229ea91eae02e1c
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-25 18:26:42 -0300

    Resolução do ex. 018

--- curso-em-video-exercicios-python/#018-seno-cosseno-e-tangente.py
+++ curso-em-video-exercicios-python/#018-seno-cosseno-e-tangente.py
@@ -1,1 +1,25 @@
-# Ex. 018 -
+# Ex. 018 - Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+# import math
+
+# angle = float(input('Digite o ângulo que você deseja: '))
+
+# sin = math.sin(math.radians(angle))
+# cos = math.cos(math.radians(angle))
+# tan = math.tan(math.radians(angle))
+
+# print(f'O ângulo de {angle} tem o SENO de {sin:.2f}')
+# print(f'O ângulo de {angle} tem o COSSENO de {cos:.2f}')
+# print(f'O ângulo de {angle} tem o TANGENTE de {tan:.2f}')
+
+from math import sin, cos, tan, radians
+
+angle = float(input('Digite o ângulo que você deseja: '))
+
+sin = sin(radians(angle))
+cos = cos(radians(angle))
+tan = tan(radians(angle))
+
+print(f'O ângulo de {angle} tem o SENO de {sin:.2f}')
+print(f'O ângulo de {angle} tem o COSSENO de {cos:.2f}')
+print(f'O ângulo de {angle} tem o TANGENTE de {tan:.2f}')
