a8281fbfae1239efa42f09c783f2137e9f29d8b7
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-28 19:18:08 -0300

    Resolução do ex. 025

--- curso-em-video-exercicios-python/#025-procurando-uma-string-dentro-de-outra.py
+++ curso-em-video-exercicios-python/#025-procurando-uma-string-dentro-de-outra.py
@@ -1,1 +1,6 @@
-# Ex. 025 -
+# Ex. 025 - Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+name = str(input('Qual é seu nome completo? ')).strip()
+
+checkingName = 'SILVA' in name.upper()
+
+print(f'Seu nome tem Silva? {checkingName}')
