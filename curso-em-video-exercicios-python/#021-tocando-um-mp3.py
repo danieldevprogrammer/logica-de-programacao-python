f2a2d55908af768e888e8e36a440a2581be9342d
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-28 19:42:11 -0300

    Resolução do ex. 021

--- curso-em-video-exercicios-python/#021-tocando-um-mp3.py
+++ curso-em-video-exercicios-python/#021-tocando-um-mp3.py
@@ -1,7 +1,8 @@
-# Ex. 021 - Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+# Ex. 021 - Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3..
+# É importante lembrar de instalar a biblioteca pygame pip3 install pygame
 import pygame
 pygame.init()
 
-pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.load('#021.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
--- curso-em-video-exercicios-python/#027-primeiro-e-ultimo-nome-de-uma-pessoa.py
+++ curso-em-video-exercicios-python/#027-primeiro-e-ultimo-nome-de-uma-pessoa.py
@@ -1,1 +1,10 @@
-# Ex. 027 -
+# Ex. 027 - Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+name = str(input('Digite seu nome completo: ')).strip()
+
+splitName = name.split()
+firstName = splitName[0]
+lastName = 
+
+print('Muito prazer em te conhecer!')
+print(f'Seu primeiro nome é {firstName}.')
+print(f'Seu último nome é {lastName}.')
