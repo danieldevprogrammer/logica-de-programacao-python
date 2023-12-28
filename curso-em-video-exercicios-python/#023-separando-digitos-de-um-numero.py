69acc5c2024e8e6e4b14c5dcb1465021e9a72a81
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-28 19:03:13 -0300

    Resolução do ex. 023

--- curso-em-video-exercicios-python/#023-separando-digitos-de-um-numero.py
+++ curso-em-video-exercicios-python/#023-separando-digitos-de-um-numero.py
@@ -1,1 +1,14 @@
-# Ex. 023 -
+# Ex. 023 - Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+number = int(input('Informe um número: '))
+
+print(f'Analisando o número {number}')
+
+unit = number // 1 % 10
+tens = number // 10 % 10
+hundreds = number // 100 % 10
+thousands = number // 1000 % 10
+
+print(f'Unidade: {unit}')
+print(f'Dezena: {tens}')
+print(f'Centena: {hundreds}')
+print(f'Milhar: {thousands}')
