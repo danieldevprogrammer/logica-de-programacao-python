3619dce395ce2f979a2d1b89a687c533623dd09f
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2023-12-28 19:30:03 -0300

    Resolução do ex. 026

--- curso-em-video-exercicios-python/#026-primeira-e-ultima-ocorrencia-de-uma-string.py
+++ curso-em-video-exercicios-python/#026-primeira-e-ultima-ocorrencia-de-uma-string.py
@@ -1,1 +1,11 @@
-# Ex. 026 -
+# Ex. 026 - Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
+phrase = str(input('Digite uma frase: ')).strip().upper()
+
+countLetterA = phrase.count('A')
+firstTimeLetterA = phrase.find('A') + 1
+lastTimeLetterA = phrase.rfind('A') + 1
+
+print(f'A letra A aparece {countLetterA} vezes na frase.')
+print(f'A primeira letra A apareceu na posição {firstTimeLetterA}.')
+print(f'A última letra A apareceu na posição {lastTimeLetterA}')
