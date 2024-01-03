69ee82350ac3fcf30f4201b953696febaf382be1
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2024-01-02 21:14:46 -0300

    Resolução do ex. 030

--- curso-em-video-exercicios-python/#030-par-ou-impar.py
+++ curso-em-video-exercicios-python/#030-par-ou-impar.py
@@ -1,1 +1,10 @@
-# Ex. 030 -
+# Ex. 030 -Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+number = int(input('Me diga um número qualquer: '))
+
+result = number % 2
+
+if result == 0:
+    print(f'O número {number} é PAR')
+else:
+    print(f'O número {number} é ÍMPAR')
