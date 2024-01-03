df4daf6cdefaa1aed61d0bd446c26242819a301e
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2024-01-03 16:46:57 -0300

    Resolução do ex. 031

--- curso-em-video-exercicios-python/#031-custo-da-viagem.py
+++ curso-em-video-exercicios-python/#031-custo-da-viagem.py
@@ -1,1 +1,14 @@
-# Ex. 031 -
+# Ex. 031 - Desenvolva um programa que pergunte a distância de uma viagem em km.
+# Calcule o preço da passagem, colocando R$0,50 por Km para viagens até 200Km e R$0,45 para viagens mais longas.
+
+tripDistance = float(input('Qual é a distância da sua viagem em Km? '))
+print(f'Você está prestes a começar uma viagem de {tripDistance}km')
+
+# triplPrice = tripDistance * 0.50 if tripDistance <= 200 else tripDistance * 0.45
+
+if tripDistance <= 200:
+    triplPrice = tripDistance * 0.50
+else:
+    triplPrice = tripDistance * 0.45
+
+print(f'E o preço da passagem será de R${triplPrice:.2f}')
