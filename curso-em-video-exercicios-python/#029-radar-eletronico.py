fbd9e827984051ef925620ba0bd5786d67673424
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2024-01-02 21:06:14 -0300

    Resolução do ex. 029

--- curso-em-video-exercicios-python/#029-radar-eletronico.py
+++ curso-em-video-exercicios-python/#029-radar-eletronico.py
@@ -1,1 +1,14 @@
-# Ex. 029 -
+# Ex. 029 - Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,
+# mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+speed = float(input('Qual a velocidade atual do carro em Km/h? '))
+
+
+if speed > 80:
+    print('MULTADO! Você excedeu o limite permitido que é de 80Km/h')
+
+    trafficTicket = (speed - 80) * 7
+    print(f'Você deve pagar uma multa de R${trafficTicket}')
+
+
+print('Tenha um bom dia! Dirija com segurança!')
