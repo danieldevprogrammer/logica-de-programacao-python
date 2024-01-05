988a7530a4530d971e44e9903b8bc0f81d421695
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2024-01-05 19:19:50 -0300

    Resolução do ex. 035

--- curso-em-video-exercicios-python/#035-analisando-triangulo-v1.0.py
+++ curso-em-video-exercicios-python/#035-analisando-triangulo-v1.0.py
@@ -1,1 +1,13 @@
-# Ex. 035 -
+# Ex. 035 - Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+print('-=' * 20)
+print('Analisador de triângulos')
+print('-=' * 20)
+
+firstSegment = float(input('Primeiro segmento: '))
+secondSegment = float(input('Segundo segmento: '))
+thirdSegment = float(input('Terceiro segmento: '))
+
+if firstSegment < secondSegment + thirdSegment and secondSegment < firstSegment + thirdSegment and thirdSegment < firstSegment + secondSegment:
+    print('Os segmentos acima PODEM FORMAR triângulo!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo!')
