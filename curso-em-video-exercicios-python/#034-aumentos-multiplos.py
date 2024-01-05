28a53740d310b7f5d87c828ceafb93c12fcda14a
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2024-01-05 19:01:44 -0300

    Resolução do ex. 034

--- curso-em-video-exercicios-python/#034-aumentos-multiplos.py
+++ curso-em-video-exercicios-python/#034-aumentos-multiplos.py
@@ -1,1 +1,13 @@
-# Ex. 034 -
+# Ex. 034 - Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+# Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+salary = float(input('Qual é o salário do funcionário? R$'))
+
+if salary <= 1250:
+    Increase = salary * 0.15
+    salaryIncrease = salary + Increase
+    print(f'Quem ganhava R${salary:.2f} passa a ganhar R${salaryIncrease}')
+else:
+    Increase = salary * 0.10
+    salaryIncrease = salary + Increase
+    print(f'Quem ganhava R${salary:.2f} passa a ganhar R${salaryIncrease}')
