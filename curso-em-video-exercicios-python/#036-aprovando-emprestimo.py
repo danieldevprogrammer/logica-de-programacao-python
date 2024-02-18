27ec0835847ce56928d3e5eafeac7cb3ca19b73f
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2024-02-18 19:44:13 -0300

    Resolução do ex. 035

---
+++ curso-em-video-exercicios-python/#036-aprovando-emprestimo.py
@@ -0,0 +1,19 @@
+# Ex. 035 - Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
+# Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+# A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado
+
+housevalue = float(input('Valor da casa: R$'))
+salary = float(input('Sálario do comprador: R$'))
+yearsOfFinancing = int(input('Quantos anos de financiamento? '))
+
+installment = housevalue / (yearsOfFinancing * 12)
+
+minimum = salary * 30 / 100
+
+print(
+    f'Para pagar uma casa de R${housevalue:.2f} em {yearsOfFinancing} anos a prestação será de R${installment:.2f}')
+
+if installment <= minimum:
+    print('Empréstimo pode ser CONCEDIDO!')
+else:
+    print('Empréstimo NEGADO!')
